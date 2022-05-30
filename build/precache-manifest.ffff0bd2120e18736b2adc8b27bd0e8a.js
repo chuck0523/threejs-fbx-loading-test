56da34b7511ba792a5e9a556643408bbe8e0a60d
Takashi Yoneyama <chuckinthestar@gmail.com>
2022-05-30 18:39:16 +0900

    build app

---
+++ build/precache-manifest.ffff0bd2120e18736b2adc8b27bd0e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b627bfb3be43f443cab4501db741de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72c607e16539218ace31",
+    "url": "/static/css/main.1a20dfa3.chunk.css"
+  },
+  {
+    "revision": "ac2bd7c44ac7afc3a483",
+    "url": "/static/js/2.24c5b6a6.chunk.js"
+  },
+  {
+    "revision": "700e7a20237744862c27fdda83660611",
+    "url": "/static/js/2.24c5b6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c607e16539218ace31",
+    "url": "/static/js/main.493a9a25.chunk.js"
+  },
+  {
+    "revision": "aa1d0f1c672be7083a94",
+    "url": "/static/js/runtime~main.afeea630.js"
+  }
+]);
